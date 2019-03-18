f29369f2a109b217a239e39f951dd807ad01419d
atibrewal98 <ankit@siplnet.com>
2019-03-19 02:22:32 +0800

    Setup App

--- basic_pages/views.py
+++ basic_pages/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 def MonkeyPageView(request):
-    return HttpResponse('Kya Bakchodi hai ye Madarchod!')
+    return render(request, 'monkey.html')
