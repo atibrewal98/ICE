67c6c1d1b8a2bdf63ab441203664ddc5038f5ee0
atibrewal98 <ankit@siplnet.com>
2019-03-20 17:54:24 +0800

    Add class to admin

--- ICE/admin.py
+++ ICE/admin.py
@@ -4,9 +4,10 @@ from .models import *
 # Register your models here.
 
 admin.site.register(LearnerAccount)
-admin.site.register(LearnerAccount)
-admin.site.register(LearnerAccount)
-admin.site.register(LearnerAccount)
-admin.site.register(LearnerAccount)
-admin.site.register(LearnerAccount)
-admin.site.register(LearnerAccount)
+admin.site.register(InstructorAccount)
+admin.site.register(Category)
+admin.site.register(Courses)
+admin.site.register(LearnerCourses)
+admin.site.register(Modules)
+admin.site.register(Components)
+admin.site.register(QuestionBank)
