1577bb758c784edbb64886bf38cdf6372ceafc39
atibrewal98 <ankit@siplnet.com>
2019-03-23 00:11:58 +0800

    Display Data

--- ICE/urls.py
+++ ICE/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
     path('', views.monkeyPageView),
     path('course/', views.courseView),
+    url(r'^course/(?P<course_ID>[0-9]+)/(?P<learner_ID>[0-9]+)/(?P<module_ID>[0-9]+)/$',views.courseView),
     path('category/', views.category_list_view),
     path('modules/', views.module_list_view),
     url(r'^modules_component/(?P<module_ID>[0-9]+)/$',views.component_list_view)
--- ICE/views.py
+++ ICE/views.py
@@ -1,17 +1,31 @@
 from django.shortcuts import render, HttpResponse
 from django.views.generic.list import ListView
 from django.template import loader
-from ICE.models import Module, Category, Component
+from ICE.models import Module, Category, Component, Course, Instructor, LearnerTakesCourse
 
 # Create your views here.
 def monkeyPageView(request):
     return render(request, 'ICE/monkey.html')
 
-def courseView(request):
-    all_modules=Module.objects.all()
+def courseView(request, course_ID, module_ID, learner_ID):
+    all_modules=Module.objects.filter(courseID = course_ID)
+    course=Course.objects.filter(courseID = course_ID)
+    instructor = ''
+    title = ''
+    components = ''
+    for c in course:
+        instructor=Instructor.objects.filter(pk = c.instructorID)
+    currModule=LearnerTakesCourse.objects.filter(courseID = course_ID, staffID = learner_ID)
+    for m in currModule:
+        title=Module.objects.filter(moduleID = m.currentModule)
+        components=Component.objects.filter(moduleID = m.currentModule)
     template=loader.get_template("ICE/courseContent.html")
     context ={
         'all_modules':all_modules,
+        'title': title,
+        'instructor': instructor,
+        'course': course,
+        'components': components
     }
     return HttpResponse(template.render(context,request))
 
