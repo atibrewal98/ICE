886e175c529aada64821d866018aecd4c3721175
atibrewal98 <ankit@siplnet.com>
2019-03-22 17:57:45 +0800

    Fix CSS Error

--- ICE/views.py
+++ ICE/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def monkeyPageView(request):
-    return render(request, 'monkey.html')
+    return render(request, 'ICE/monkey.html')
 
 def courseView(request):
-    return render(request, 'courseContent.html')
+    return render(request, 'ICE/courseContent.html')
