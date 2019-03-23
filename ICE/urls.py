3bfb8a9a4795a06beeb26183991362ab6b99221e
atibrewal98 <ankit@siplnet.com>
2019-03-23 17:11:08 +0800

    Fix URL structure

--- ICE/urls.py
+++ ICE/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
     path('', views.monkeyPageView),
     path('course/', views.courseView),
-    url(r'^course/(?P<course_ID>[0-9]+)/(?P<learner_ID>[0-9]+)/(?P<module_ID>[0-9]+)/$',views.courseView),
+    url(r'^course/courseID=(?P<course_ID>[0-9]+)&learnerID=(?P<learner_ID>[0-9]+)&moduleID=(?P<module_ID>[0-9]+)/$',views.courseView),
     path('category/', views.category_list_view),
     path('modules/', views.module_list_view),
     path('postmodule/', views.module_form),
