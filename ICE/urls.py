019a9c08435a48595c9e179add57b947d9ff1fa2
atibrewal98 <ankit@siplnet.com>
2019-04-09 04:04:59 +0800

    Fix URL Issues

--- ICE/urls.py
+++ ICE/urls.py
@@ -9,19 +9,17 @@ urlpatterns = [
     url(r'^moduleQuiz/(?P<id>[0-9]+)/$', views.intructor_view_quiz),
     url(r'category/categoryID=(?P<category_id>[0-9]+)/$', views.category_list_view),
     url(r'^courseDescription/courseID=(?P<course_id>[0-9]+)/$', views.courseDescriptionView),
-    # path('modules/', views.module_list_view),
     url(r'^addCourse/$', views.course_form),
     url(r'^addModule/courseID=(?P<course_id>[0-9]+)/$', views.module_form),
     url(r'^addModuleQuiz/(?P<id>[0-9]+)/$',views.quiz_form),
     url(r'^addComponent/moduleID=(?P<module_id>[0-9]+)/',views.component_form),
-    path('comp/', views.component),
-    # url(r'^modules_component/(?P<module_ID>[0-9]+)/$',views.component_list_view),
+    url(r'comp/', views.component),
     url(r'^dashboard/$',views.course_learner_view, name="course_learner"),
     url(r'^instructorDashboard/$',views.course_instructor_view, name="course_instructor"),
     url(r'^quiz/qID=(?P<q_ID>[0-9]+)$', views.learner_quiz),
-    path('accounts/', include('django.contrib.auth.urls')),
+    url(r'accounts/', include('django.contrib.auth.urls')),
     url(r'login_success/$', views.login_success, name='login_success'),
-    path('invite/', views.invite, name='invite'), #for admin to invite instructors
-    path('learner_get_token/', views.learner_get_token, name='learner_get_token'), #for learner to get token
-    path('signup/<uidb64>/<token>', views.signup, name='signup'),
+    url(r'invite/', views.invite, name='invite'), #for admin to invite instructors
+    url(r'learner_get_token/', views.learner_get_token, name='learner_get_token'), #for learner to get token
+    url(r'signup/<uidb64>/<token>', views.signup, name='signup'),
 ]
